e990e4b59857dfeabe4e82adf8b7adc8e33785d3
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-05 22:23:41 +0900

    added gateway response default 4xx

--- serverless.ts
+++ serverless.ts
@@ -26,6 +26,26 @@ const serverlessConfiguration: AWS = {
   },
   // import the function via paths
   functions: { register, auth },
+  resources: {
+    Resources: {
+      GatewayResponseDefault4XX: {
+        Type: 'AWS::ApiGateway::GatewayResponse',
+        Properties: {
+          ResponseParameters: {
+            'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+            'gatewayresponse.header.Access-Control-Allow-Headers':
+              "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+            'gatewayresponse.header.Access-Control-Allow-Methods':
+              "'GET,OPTIONS,POST'",
+          },
+          ResponseType: 'DEFAULT_4XX',
+          RestApiId: {
+            Ref: 'ApiGatewayRestApi',
+          },
+        },
+      },
+    },
+  },
 };
 
 module.exports = serverlessConfiguration;
