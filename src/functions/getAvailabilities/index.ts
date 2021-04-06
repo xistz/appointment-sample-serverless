76161be6fa44bf51f046a986abb98fc4c086f565
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-06 23:43:53 +0900

    added schema

--- src/functions/getAvailabilities/index.ts
+++ src/functions/getAvailabilities/index.ts
@@ -1,4 +1,5 @@
 import { handlerPath } from '@libs/handlerResolver';
+import schema from './schema';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
@@ -10,6 +11,9 @@ export default {
         path: 'availabilities',
         cors: true,
         request: {
+          schemas: {
+            'application/json': schema,
+          },
           parameters: {
             querystrings: {
               from: true,
