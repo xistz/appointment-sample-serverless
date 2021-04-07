d2cce24fccf7676447370ba5c3c19a5ee6aad24b
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-07 21:40:13 +0900

    debug

--- src/functions/getAvailabilities/index.ts
+++ src/functions/getAvailabilities/index.ts
@@ -1,5 +1,4 @@
 import { handlerPath } from '@libs/handlerResolver';
-import schema from './schema';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
@@ -11,9 +10,9 @@ export default {
         path: 'availabilities',
         cors: true,
         request: {
-          schemas: {
-            'application/json': schema,
-          },
+          // schemas: {
+          //   'application/json': schema,
+          // },
           parameters: {
             querystrings: {
               from: true,
