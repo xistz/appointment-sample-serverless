5865c20eb095a7173f2b8c22cac1df4df349d843
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-07 21:10:59 +0900

    mock listAvailabilities

--- src/functions/getAvailabilities/handler.ts
+++ src/functions/getAvailabilities/handler.ts
@@ -5,6 +5,7 @@ import {
 import { getUser, User } from '@libs/Jwt';
 import { middyfy } from '@libs/lambda';
 import { createLogger } from '@libs/logger';
+import { listAvailabilities } from '@services/availability';
 import 'source-map-support/register';
 import schema from './schema';
 
@@ -14,6 +15,7 @@ const getAvailabilities: ValidatedEventQueryAPIGatewayProxyEvent<
   typeof schema
 > = async (event) => {
   const user = getUser(event.headers.Authorization);
+  const { from, to } = event.queryStringParameters;
 
   if (!isFP(user)) {
     return formatJSONResponse(
@@ -25,6 +27,10 @@ const getAvailabilities: ValidatedEventQueryAPIGatewayProxyEvent<
   }
 
   logger.info(`getting availabilities for financial planner`);
+
+  const availabilities = await listAvailabilities(user.id, from, to);
+
+  return formatJSONResponse({ data: availabilities });
 };
 
 export const main = middyfy(getAvailabilities);
--- src/services/availability.ts
+++ src/services/availability.ts
@@ -1,1 +1,27 @@
-export function listAvailabilities(fpId: string, from: string, to: string) {}
+interface Availability {
+  id: string;
+  attributes: {
+    from: string;
+  };
+}
+
+export function listAvailabilities(
+  fpId: string,
+  from: string,
+  to: string
+): Availability[] {
+  return [
+    {
+      id: 'random_id_1',
+      attributes: {
+        from: new Date(2020, 3, 7, 10).toISOString(),
+      },
+    },
+    {
+      id: 'random_id_2',
+      attributes: {
+        from: new Date(2020, 3, 7, 10, 30).toISOString(),
+      },
+    },
+  ];
+}
