817340dcc94aa5765c1059adc600603ab211320c
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-08 23:53:32 +0900

    added create and delete to availability service

--- src/services/availability.ts
+++ src/services/availability.ts
@@ -1,27 +1,43 @@
-interface Availability {
-  id: string;
-  attributes: {
-    from: string;
-  };
-}
+import { Availability } from '@models/availability';
+import { AvailabilitiesDB } from '@databases/availabilities';
 
-export function listAvailabilities(
+const availabilitiesDB = new AvailabilitiesDB();
+
+export async function listAvailabilities(
   fpId: string,
   from: string,
   to: string
-): Availability[] {
-  return [
-    {
-      id: 'random_id_1',
-      attributes: {
-        from: new Date(2021, 3, 7, 1).toISOString(),
-      },
-    },
-    {
-      id: 'random_id_2',
-      attributes: {
-        from: new Date(2021, 3, 7, 1, 30).toISOString(),
-      },
-    },
-  ];
+): Promise<Availability[]> {
+  // return [
+  //   {
+  //     id: 'random_id_1',
+  //     fpId: 'random_fp_id',
+  //     from: new Date(2021, 3, 7, 1).toISOString(),
+  //   },
+  //   {
+  //     id: 'random_id_2',
+  //     fpId: 'random_fp_id',
+  //     from: new Date(2021, 3, 7, 1, 30).toISOString(),
+  //   },
+  // ];
+
+  const availabilities = await availabilitiesDB.list(fpId, from, to);
+
+  return availabilities;
+}
+
+export async function createAvailability(
+  fpId: string,
+  from: string
+): Promise<Availability['id']> {
+  const id = await availabilitiesDB.create(fpId, from);
+
+  return id;
+}
+
+export async function deleteAvailability(
+  id: string,
+  fpId: string
+): Promise<void> {
+  await availabilitiesDB.delete(id, fpId);
 }
