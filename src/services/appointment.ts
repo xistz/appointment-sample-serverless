8fcd9027291b95a407e306d76c221e5654448857
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-14 22:48:40 +0900

    stub createAppointment

---
+++ src/services/appointment.ts
@@ -0,0 +1,12 @@
+import { AvailabilitiesDB } from '@databases/availabilities';
+
+const availabilitiesDB = new AvailabilitiesDB();
+
+export async function createAppointment(
+  clientId: string,
+  availabilityId: string
+): Promise<string> {
+  const id = '';
+
+  return id;
+}
