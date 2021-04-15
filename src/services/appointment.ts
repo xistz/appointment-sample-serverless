5136ad4242637e54fad13c942bc90a1577b38958
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-15 23:17:32 +0900

    added listAppointments

--- src/services/appointment.ts
+++ src/services/appointment.ts
@@ -1,4 +1,6 @@
 import { AvailabilitiesDB } from '@databases/availabilities';
+import { AppointmentDetails } from '@models/appointment';
+import { getUsers } from './user';
 
 const availabilitiesDB = new AvailabilitiesDB();
 
@@ -14,4 +16,38 @@ export async function deleteAppointment(
   userId: string
 ): Promise<void> {
   await availabilitiesDB.deleteAppointment(availabilityId, userId);
+}
+
+export async function listAppointments(
+  userId: string,
+  from: string,
+  to: string
+): Promise<AppointmentDetails[]> {
+  const appointments = await availabilitiesDB.listAppointments(
+    userId,
+    from,
+    to
+  );
+
+  const userIds = new Set<string>();
+  appointments.forEach((appointment) =>
+    userIds.add(appointment.clientId).add(appointment.fpId)
+  );
+
+  const users = await getUsers(Array.from(userIds));
+
+  const appointmentDetails = appointments.map((appointment) => {
+    const { id, from } = appointment;
+    const fp = users[appointment.fpId];
+    const client = users[appointment.clientId];
+
+    return {
+      id,
+      from,
+      fp,
+      client,
+    };
+  });
+
+  return appointmentDetails;
 }
