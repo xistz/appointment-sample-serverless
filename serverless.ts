a83df7eef1236f65e4f8d3ec475bc8a4bd10052a
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-20 00:02:07 +0900

    fixed cors

--- serverless.ts
+++ serverless.ts
@@ -64,8 +64,7 @@ const serverlessConfiguration: AWS = {
         Properties: {
           ResponseParameters: {
             'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
-            'gatewayresponse.header.Access-Control-Allow-Headers':
-              "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+            'gatewayresponse.header.Access-Control-Allow-Headers': "'*'",
             'gatewayresponse.header.Access-Control-Allow-Methods':
               "'GET,OPTIONS,POST'",
           },
