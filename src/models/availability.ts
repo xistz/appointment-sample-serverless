f01bd95f8a41b1f9487cbbecc588175a6c0271be
Chan Jonathan <chan.jonathan@moneyforward.co.jp>
2021-04-20 23:41:27 +0900

    added AvailabilityFP

--- src/models/availability.ts
+++ src/models/availability.ts
@@ -1,9 +1,10 @@
-export interface Availability {
-  id: string;
-  fpId: string;
-  from: string;
-}
+export interface Availability extends AvailabilityTime, AvailabilityFP {}
 
 export interface AvailabilityTime {
   from: string;
+}
+
+export interface AvailabilityFP {
+  id: string;
+  fpId: string;
 }
